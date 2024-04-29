22167b4504b6b7a35944ef53d6be5884ef1e5d72
Nigel Santiago <166557965+nigelsantiago@users.noreply.github.com>
2024-04-29 12:31:22 -0700

    added cpp Engine file

---
+++ code/Engine.cpp
@@ -0,0 +1,19 @@
+#include "Engine.h"
+using namespace sf;
+using namespace std;
+
+Engine::Engine()
+{
+}
+void Engine::input()
+{
+}
+void Engine::update(float dtAsSeconds)
+{
+}
+void Engine::draw()
+{
+}
+void Engine::run()
+{
+}
