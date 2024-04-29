f185b535f5623cdc615ec6a2aab3895c8afb86f8
Nigel Santiago <166557965+nigelsantiago@users.noreply.github.com>
2024-04-29 16:02:00 -0700

    Update Matrices.h not final

--- code/Matrices.h
+++ code/Matrices.h
@@ -78,7 +78,11 @@ namespace Matrices
             sin(theta)   cos(theta)
             */
             ///theta represents the angle of rotation in radians, counter-clockwise
-            RotationMatrix(double theta);
+            RotationMatrix(double theta)
+            : Matrix (2, 2)
+            {
+                
+            }
     };
 
     ///2D scaling matrix
