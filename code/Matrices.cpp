f4dc0acb39796b30f7c760ae5d7a91830bba63d3
Nigel Santiago <166557965+nigelsantiago@users.noreply.github.com>
2024-04-30 15:49:19 -0700

    matrix constructor is it in the right spot?

--- code/Matrices.cpp
+++ code/Matrices.cpp
@@ -129,5 +129,29 @@ namespace Matrices
     }
     return os;
   }
-}
 
+    TranslationMatrix::TranslationMatrix(double xShift, double yShift, int nCols)
+      : Matrix (2, nCols)
+  {
+    for (int j = 0; j < nCols; j++)
+      (*this)(0, j) = xShift;
+    for (int j = 0; j < nCols; j++)
+      (*this)(1, j) = yShift;
+  }
+    ScalingMatrix::ScalingMatrix(double scale)
+      : Matrix (2, 2)
+  {
+    (*this)(0, 0) = scale;
+    (*this)(1, 0) = 0;
+    (*this)(0, 1) = 0;
+    (*this)(1, 1) = scale;
+  }
+  RotationMatrix::RotationMatrix(double theta)
+      : Matrix (2, 2)
+  {
+    (*this)(0, 0) = cos(theta);
+    (*this)(1, 0) = sin(theta) * -1;
+    (*this)(0, 1) = sin(theta);
+    (*this)(1, 1) = cos(theta);
+  }
+}
